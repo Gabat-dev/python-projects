851674c69d6c0ab602884d87108d5422f766ab5b
Gabat-dev <eniolaganikale@gmail.com>
2026-07-10 18:20:51 +0100

    Add files via upload

---
+++ honeypot.py
@@ -0,0 +1,24 @@
+import socket
+import datetime
+import logging
+import time
+
+logging.basicConfig(filename="honeypot.log", level=logging.INFO, format="%(message)s")
+def start_honeypot(port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("0.0.0.0",port))
+    server.listen(5)
+    print(f"honeypot running on port {port}...")
+    print("waiting for attackers....\n")
+
+    while True:
+        client, address = server.accept()
+        client.send("welcome to SSH server v1.0\r\n".encode())
+        time.sleep(10)
+        data = client.recv(1024).decode()
+        timestamp = datetime.datetime.now()
+        logging.info(f"connect from {address[0]} in { data} at {timestamp}")
+        client.close()
+
+start_honeypot(9999)
